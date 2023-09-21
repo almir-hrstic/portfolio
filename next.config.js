678af68bf8e6ca64d2d3841d9cdffcc263866c0c
Almir Hrštić <almirhrstic@gmail.com>
2023-09-22 01:37:40 +0200

    22.09.2023

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
 const nextConfig = {
 
   output: 'export',
-  reactStrictMode: true,
 
   env: {
-    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+
+    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL
   }
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,17 @@ import { Inter } from 'next/font/google'
 
 const fonts = Inter({ subsets: ['latin'] })
 
+export async function generateMetadata() {
+
+  const data = await fetch(`${process.env.BASE_URL}/meta.json`).then(response => response.json())
+
+  return {
+
+    title: data.title,
+    description: data.description
+  }
+}
+
 export default function Layout({ children }) {
 
   return (
