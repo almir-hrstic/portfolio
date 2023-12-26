192b5a4551d3175295a0c1ea2d57205214a92f25
Almir Hrštić <almirhrstic@gmail.com>
2023-12-26 17:19:53 +0100

    26.12.2023

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,15 +13,15 @@ export default function Layout({ children }) {
 
       <head>
 
-        <link rel="apple-touch-icon" sizes="180x180" href={`${process.env.BASE_URL}/pwa/apple-touch-icon.png`} />
-        <link rel="icon" type="image/png" sizes="32x32" href={`${process.env.BASE_URL}/pwa/favicon-32x32.png`} />
-        <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.BASE_URL}/pwa/favicon-16x16.png`} />
-        <link rel="manifest" href={`${process.env.BASE_URL}/pwa/site.webmanifest`} />
-        <link rel="mask-icon" href={`${process.env.BASE_URL}/pwa/safari-pinned-tab.svg`} color="#092443" />
-        <link rel="shortcut icon" href={`${process.env.BASE_URL}/pwa/favicon.ico`} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${process.env.BASE_URL}/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${process.env.BASE_URL}/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.BASE_URL}/favicon-16x16.png`} />
+        <link rel="manifest" href={`${process.env.BASE_URL}/site.webmanifest`} />
+        <link rel="mask-icon" href={`${process.env.BASE_URL}/safari-pinned-tab.svg`} color="#092443" />
+        <link rel="shortcut icon" href={`${process.env.BASE_URL}/favicon.ico`} />
 
         <meta name="msapplication-TileColor" content="#092443" />
-        <meta name="msapplication-config" href={`${process.env.BASE_URL}/pwa/browserconfig.xml`} />
+        <meta name="msapplication-config" href={`${process.env.BASE_URL}/browserconfig.xml`} />
         <meta name="theme-color" content="#092443" />
 
       </head>
