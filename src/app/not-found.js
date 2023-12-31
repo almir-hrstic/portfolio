fcf9e2321d7af3b9e12a8d958f6af7d68c8ae781
Almir Hrštić <almirhrstic@gmail.com>
2023-12-31 15:38:06 +0100

    31.12.2023

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,14 @@
 import Container from "../components/container"
 import Error404 from '../components/error-404'
 
+export const generateMetadata = async () => {
+
+  let data = await fetch(`${process.env.BASE_URL}/404-meta.json`).then(response => response.json())
+  
+
+  return data
+}
+
 export default function NotFound() {
 
   return (
