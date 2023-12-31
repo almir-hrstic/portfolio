755c4f22f6d37e5ff68427b524e73c22fc9c6816
Almir Hrštić <almirhrstic@gmail.com>
2023-12-31 15:06:57 +0100

    31.12.2023

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,12 @@
 import Container from "../components/container"
 import Error404 from '../components/error-404'
 
-export const generateMetadata = async () => {
+export async function generateMetadata() {
 
-  return await fetch(`${process.env.BASE_URL}/meta.json`).then(response => response.json())
+  let data = await fetch(`${process.env.BASE_URL}/meta.json`).then(response => response.json())
+
+  return { title: `Page not found | ${data.title.default}` }
 }
-
 
 export default function NotFound() {
 
