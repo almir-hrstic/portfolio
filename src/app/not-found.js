ba680d193c7b04eb56720fd8be6f986ef43714b8
Almir Hrštić <almirhrstic@gmail.com>
2023-12-31 23:42:45 +0100

    31.12.2023

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,4 +1,4 @@
-import Container from "../components/container"
+import Container from '../components/container'
 import Error404 from '../components/error-404'
 
 export const metadata = { title: 'Page not found | Almir Hrštić' }
