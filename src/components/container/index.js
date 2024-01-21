e525cc15b98369bcb1a942b272bf3c357d12ec22
Almir Hrštić <almirhrstic@gmail.com>
2024-01-21 18:03:11 +0100

    21.01.2024

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -11,25 +11,12 @@ export default function Container({ children }) {
   const root = useRef()
 
   const setScreenHeight = () => root.current.style.setProperty('--screen-height', `${window.innerHeight - .01}px`)
-  const clearMousePosition = () => window.innerWidth >= 1024 ? false : root.current.style.removeProperty('background')
-
-  const setMousePosition = (event) => {
-
-    if (window.innerWidth < 1024) return false
-
-    root.current.style.setProperty(
-
-      'background',
-      `radial-gradient(640px at ${event.pageX}px ${event.pageY}px, var(--color-blue-light), transparent 75%)`
-    )
-  }
 
   const setResize = debounce(() => {
 
     if (!resized()) return false
 
     setScreenHeight()
-    clearMousePosition()
   })
 
   useEffect(() => {
@@ -43,7 +30,7 @@ export default function Container({ children }) {
 
   return (
 
-    <div className={styles.root} ref={root} onMouseMove={event => setMousePosition(event)}>
+    <div className={styles.root} ref={root}>
       {children}
     </div>
   )
